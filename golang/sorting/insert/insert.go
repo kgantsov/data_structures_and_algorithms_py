78f94f973a20e52ab29fc13e271904dd7d271f54
Konstantin Hantsov <k.gantsov@gmail.com>
2017-05-15 18:16:55 +0200

    Golang: Implementation of insertion sort

---
+++ golang/sorting/insert/insert.go
@@ -0,0 +1,15 @@
+package insert
+
+func Sort(list []int) {
+	for i := 1; i < len(list); i++ {
+		value := list[i]
+		j := i - 1
+
+		for j >= 0 && list[j] > value {
+			list[j+1] = list[j]
+			j--
+		}
+
+		list[j+1] = value
+	}
+}
