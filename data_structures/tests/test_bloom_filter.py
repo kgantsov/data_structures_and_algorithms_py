9e261c54724b91b5d5745a7fb3fc2a9af06dc9c9
Konstantin Hantsov <k.gantsov@gmail.com>
2018-07-10 19:56:02 +0200

    Add BloomFilter underlying table size

--- data_structures/tests/test_bloom_filter.py
+++ data_structures/tests/test_bloom_filter.py
@@ -6,6 +6,55 @@ from data_structures.bloom_filter import BloomFilter
 
 class TestBloomFilter(TestCase):
 
+    def test_size(self):
+        b = BloomFilter(probability=0.1, n=2)
+        self.assertEqual(len(b.hash_table), 10)
+
+        b = BloomFilter(probability=0.1, n=5)
+        self.assertEqual(len(b.hash_table), 24)
+
+        b = BloomFilter(probability=0.1, n=10)
+        self.assertEqual(len(b.hash_table), 48)
+
+        b = BloomFilter(probability=0.1, n=100)
+        self.assertEqual(len(b.hash_table), 480)
+
+        b = BloomFilter(probability=0.1, n=1000)
+        self.assertEqual(len(b.hash_table), 4793)
+
+        b = BloomFilter(probability=0.01, n=2)
+        self.assertEqual(len(b.hash_table), 20)
+
+        b = BloomFilter(probability=0.01, n=5)
+        self.assertEqual(len(b.hash_table), 48)
+
+        b = BloomFilter(probability=0.01, n=10)
+        self.assertEqual(len(b.hash_table), 96)
+
+        b = BloomFilter(probability=0.01, n=100)
+        self.assertEqual(len(b.hash_table), 959)
+
+        b = BloomFilter(probability=0.01, n=1000)
+        self.assertEqual(len(b.hash_table), 9586)
+
+        b = BloomFilter(probability=0.0005, n=2)
+        self.assertEqual(len(b.hash_table), 32)
+
+        b = BloomFilter(probability=0.0005, n=5)
+        self.assertEqual(len(b.hash_table), 80)
+
+        b = BloomFilter(probability=0.0005, n=10)
+        self.assertEqual(len(b.hash_table), 159)
+
+        b = BloomFilter(probability=0.0005, n=100)
+        self.assertEqual(len(b.hash_table), 1583)
+
+        b = BloomFilter(probability=0.0005, n=1000)
+        self.assertEqual(len(b.hash_table), 15821)
+
+        b = BloomFilter(probability=0.0005, n=5000)
+        self.assertEqual(len(b.hash_table), 79102)
+
     def test_set(self):
         b = BloomFilter()
 
@@ -13,14 +62,14 @@ class TestBloomFilter(TestCase):
         self.assertFalse(b.check("user:165"))
         self.assertFalse(b.check("user:55"))
         self.assertFalse(b.check("user:99"))
-        
+
         b.add("user:35")
-        
+
         self.assertTrue(b.check("user:35"))
         self.assertFalse(b.check("user:165"))
         self.assertFalse(b.check("user:55"))
         self.assertFalse(b.check("user:99"))
-    
+
         b.add("user:165")
         self.assertTrue(b.check("user:35"))
         self.assertTrue(b.check("user:165"))
