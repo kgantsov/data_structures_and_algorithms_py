b9a0e352cacd4055d0d92f7c93dc317c6fdaeb19
Konstantin Hantsov <k.gantsov@gmail.com>
2018-07-17 21:05:08 +0200

    Changed bloom filter's list to an array

--- data_structures/bloom_filter.py
+++ data_structures/bloom_filter.py
@@ -1,3 +1,4 @@
+import array
 import math
 
 
@@ -9,7 +10,7 @@ class BloomFilter:
             (capacity * math.log(probability)) / math.log(1 / math.pow(2,  math.log(2)))
         )
 
-        self.hash_table = [False] * self.size
+        self.hash_table = array.array('b', [0] * self.size)
         self.hashes_num = round(math.log(2) * self.size / capacity)
 
     def hash_func(self, key, seed=0):
@@ -17,7 +18,7 @@ class BloomFilter:
 
     def add(self, key):
         for seed in range(self.hashes_num):
-            self.hash_table[self.hash_func(key, seed)] = True
+            self.hash_table[self.hash_func(key, seed)] = 1
 
     def check(self, key):
         return all([
