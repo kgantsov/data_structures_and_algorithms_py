478c1015be802edc26741b851917877713978630
Konstantin Hantsov <k.gantsov@gmail.com>
2018-07-22 22:31:17 +0200

    Add some tests that chech lenth of the bloom filter

--- data_structures/tests/test_bloom_filter.py
+++ data_structures/tests/test_bloom_filter.py
@@ -10,50 +10,82 @@ class TestBloomFilter(TestCase):
         b = BloomFilter(probability=0.1, capacity=2)
         self.assertEqual(len(b.hash_table), 10)
 
+        self.assertEqual(len(b), 2)
+
         b = BloomFilter(probability=0.1, capacity=5)
         self.assertEqual(len(b.hash_table), 24)
+
+        self.assertEqual(len(b), 5)
 
         b = BloomFilter(probability=0.1, capacity=10)
         self.assertEqual(len(b.hash_table), 48)
 
+        self.assertEqual(len(b), 10)
+
         b = BloomFilter(probability=0.1, capacity=100)
         self.assertEqual(len(b.hash_table), 480)
+        
+        self.assertEqual(len(b), 100)
 
         b = BloomFilter(probability=0.1, capacity=1000)
         self.assertEqual(len(b.hash_table), 4793)
 
+        self.assertEqual(len(b), 1000)
+
         b = BloomFilter(probability=0.01, capacity=2)
         self.assertEqual(len(b.hash_table), 20)
+
+        self.assertEqual(len(b), 2)
 
         b = BloomFilter(probability=0.01, capacity=5)
         self.assertEqual(len(b.hash_table), 48)
 
+        self.assertEqual(len(b), 5)
+
         b = BloomFilter(probability=0.01, capacity=10)
         self.assertEqual(len(b.hash_table), 96)
+
+        self.assertEqual(len(b), 10)
 
         b = BloomFilter(probability=0.01, capacity=100)
         self.assertEqual(len(b.hash_table), 959)
 
+        self.assertEqual(len(b), 100)
+
         b = BloomFilter(probability=0.01, capacity=1000)
         self.assertEqual(len(b.hash_table), 9586)
+
+        self.assertEqual(len(b), 1000)
 
         b = BloomFilter(probability=0.0005, capacity=2)
         self.assertEqual(len(b.hash_table), 32)
 
+        self.assertEqual(len(b), 2)
+
         b = BloomFilter(probability=0.0005, capacity=5)
         self.assertEqual(len(b.hash_table), 80)
+
+        self.assertEqual(len(b), 5)
 
         b = BloomFilter(probability=0.0005, capacity=10)
         self.assertEqual(len(b.hash_table), 159)
 
+        self.assertEqual(len(b), 10)
+
         b = BloomFilter(probability=0.0005, capacity=100)
         self.assertEqual(len(b.hash_table), 1583)
+
+        self.assertEqual(len(b), 100)
 
         b = BloomFilter(probability=0.0005, capacity=1000)
         self.assertEqual(len(b.hash_table), 15821)
 
+        self.assertEqual(len(b), 1000)
+
         b = BloomFilter(probability=0.0005, capacity=5000)
         self.assertEqual(len(b.hash_table), 79102)
+
+        self.assertEqual(len(b), 5000)
 
     def test_set(self):
         b = BloomFilter()
